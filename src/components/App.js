7eacbf38b46f9e276d9491d1647f1bc461e41e9b
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-04-08 13:13:58 +0530

    str1

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from "react";
-
+function Garage() {
+  return (
 <p>
   Now I can render any React component on any DOM node I want using
   ReactDOM.render
 </p>;
+ );
+}
 
 export default App;
